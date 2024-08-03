85e8fa4371455590f1ecd5e0933e5b20287c55cf
thduy2003 <duytrieudong@gmail.com>
2024-08-04 00:08:17 +0700

    state

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState<number>(0);
   return (
     <View style={styles.container}>
       <Text>Hello world!!</Text>
       <Text style={styles.hello}>Hello world2</Text>
+      <View>
+        <Text>{count}</Text>
+        <Button title='Increase' onPress={() => setCount(count + 1)} />
+      </View>
     </View>
   );
 }
